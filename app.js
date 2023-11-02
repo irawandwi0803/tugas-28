67a1029ddec1bfc260d840bdd8bbe2d9792f200d
irawandwi0803 <dwiirawan0803@gmail.com>
2023-11-02 15:05:07 +0700

    commit pertama

--- app.js
+++ app.js
@@ -3,22 +3,33 @@ const res = require("express/lib/response");
 const req = require("express/lib/request");
 const app = express();
 const port = 3000;
+const contacts = require('./utility/contacts')
+
+// menggunakan ejs
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.sendFile('./index.html', {
-        root: __dirname
-    });
+    // res.sendFile('./index.html', {
+    //     root: __dirname
+    // });
+    res.render('index');
 });
 
 app.get('/about', (req, res) => {
-    res.sendFile('./about.html', { 
-        root: __dirname
-    });
+    // res.sendFile('./about.html', { 
+    //     root: __dirname
+    // });
+    res.render('about');
 });
 
 app.get('/contact', (req, res) => {
-    res.sendFile('./contact.html', {
-        root: __dirname
+    // res.sendFile('./contact.html', {
+    //     root: __dirname
+    // });
+    
+    const contact = contacts.loadContact();
+    res.render('contact', {
+        contacts: contact
     });
 });
 
---
+++ utility/contacts.js
@@ -0,0 +1,23 @@
+const fs = require(`fs`);
+
+// cek dan membuat folder jika tidak ada
+const dirPath = `./data`;
+if (!fs.existsSync(dirPath)) {
+    fs.mkdirSync(dirPath)
+};
+
+// cek dan membuat file data kontak jika tidak ada
+const dataPath = `data/contacts.json`;
+if (!fs.existsSync(dataPath)) {
+    fs.writeFileSync(dataPath, `[]`, `utf-8`)
+};
+
+// load kontak
+const loadContact = () => {
+    const file = fs.readFileSync(dataPath, `utf-8`);
+    const contacts = JSON.parse(file);
+    return contacts;
+}
+
+
+module.exports = { loadContact }
